cb8445edcb2a8eae49f2307d1d96800f33e2a09d
Giuseppe Rossi <giuseppe.rossi1@unicredit.eu>
2017-01-07 02:16:40 +0100

    Gestione fallback3

--- worker.js
+++ worker.js
@@ -4,7 +4,8 @@ self.addEventListener('install', function(event) {
   console.log("SW installed");
   caches.open('cache-v1').then(function(cache) {
   	return cache.addAll([
-  		'/logo_ucresearch.png'
+  		'/logo_ucresearch.png',
+  		'/offline.html'
   		]);
   })
 });
@@ -17,6 +18,8 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
 	//event.respondWith(new Response("Hijacked Site!!"));
+	var requestURL = new URL(event.request.url);
+
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
 		  return response || fetch(event.request);
